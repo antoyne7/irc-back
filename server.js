c4d2764226316a5dee124692ceca2e96075862c0
Antoine Braillard <antoine@elixir.health>
2021-02-07 00:53:29 +0100

    save

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // };
 
 const corsOptions = {
-    origin: ["https://irc-fron.vercel.app", "http://localhost:3000"],
+    origin: ["https://ircal-mern.vercel.app/", "http://localhost:3000"],
 };
 
 const server = http.createServer(app);
